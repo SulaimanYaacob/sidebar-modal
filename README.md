73f0634b3303fafc653bf2ff2579b1bc4989ca72
Sulaiman Yaacob <davidjethro01@gmail.com>
2021-07-17 16:03:01 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+"# sidebar-modal" 
+"# sidebar-modal" 
+"# sidebar-modal" 
---
+++ src/context.js
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  ); // children is crucial (this is AppContext)
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}; // now you dont need to repeatedly declare = useContext(AppContext) instead just import useGlobalContext
+
+export { AppContext, AppProvider };
